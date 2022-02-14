ceec70e36b424b2ad45f6d8016a9ae7d8ab05686
Kabeer Ahmad <55357150+realkabeerahmad@users.noreply.github.com>
2022-02-15 00:03:51 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,48 @@
-let main = document.getElementById("_main");
-console.log(document.body.style.width);
-function openNav() {
-	document.getElementById("close").style.display = "block";
-	document.getElementById("open").style.display = "none";
-	document.getElementById("mySidebar").style.height = "112px";
-	main.style.marginTop = "112px";
-}
+window.addEventListener("scroll", reveal);
+$(function () {
+  $(window).on("scroll", function () {
+    var WindowTop = $(window).scrollTop();
+    $("section").each(function (i) {
+      if (
+        WindowTop > $(this).offset().top - 100 &&
+        WindowTop < $(this).offset().top + $(this).outerHeight(true)
+      ) {
+        $(".navlink").removeClass("active");
+        $(".navbar li").eq(i).find("a").addClass("active");
+      }
+    });
+  });
 
-function closeNav() {
-	document.getElementById("close").style.display = "none";
-	document.getElementById("open").style.display = "block";
-	document.getElementById("mySidebar").style.height = "0";
-	main.style.marginTop = "0";
+  $("a[href*=\\#]:not([href=#])").click(function () {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        $("html,body").animate(
+          {
+            scrollTop: target.offset().top,
+          },
+          1000
+        );
+        return false;
+      }
+    }
+  });
+});
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 50;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("activate");
+    } else {
+      reveals[i].classList.remove("activate");
+    }
+  }
 }
